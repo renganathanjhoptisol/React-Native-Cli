4dbbb8f1a878f75707d21b64412c4fef2b2c5395
renganathanjhoptisol <renganathan.jh@optisolbusiness.com>
2021-06-22 17:23:08 +0530

    updated src

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import _RefreshControl from './assets/components/_RefreshControl';
+import _RefreshControl from './src/components/_RefreshControl';
 
 export default class App extends Component {
   render () {
